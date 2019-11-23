022f5b512630557ee02f24a4041d03590eeda3d9
adandrew <a.daniel.andrew@gmail.com>
2019-11-22 21:41:09 -0500

    Exception Handler and Discount Calculation

--- src/main/java/com/pf/entry/ShoppingClient.java
+++ src/main/java/com/pf/entry/ShoppingClient.java
@@ -1,9 +1,25 @@
 package com.pf.entry;
 
+import com.pf.service.DiscountService;
+import com.pf.service.impl.DiscountServiceImpl;
+
 public class ShoppingClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DiscountService discountService = new DiscountServiceImpl();
+		discountService.freeOffer(1, 1, 1, 1);
+		discountService.freeOffer(1, 1, 2, 1);
+		discountService.freeOffer(1, 1, 3, 1);
+		discountService.freeOffer(1, 1, 4, 1);
+		discountService.freeOffer(1, 1, 5, 1);
+		discountService.freeOffer(1, 1, 6, 1);
+		
+		discountService.freeOffer(2, 1, 1, 2);
+		discountService.freeOffer(2, 1, 2, 2);
+		discountService.freeOffer(2, 1, 3, 2);
+		discountService.freeOffer(2, 1, 4, 2);
+		discountService.freeOffer(2, 1, 5, 2);
+		discountService.freeOffer(2, 1, 6, 2);
 
 	}
 
--- src/main/java/com/pf/service/CheckoutService.java
+++ src/main/java/com/pf/service/CheckoutService.java
@@ -1,5 +1,13 @@
 package com.pf.service;
 
+import java.util.List;
+
+import com.pf.domain.Product;
+
 public interface CheckoutService {
+	
+	public void addItems(List<Product> products);
+	public void updateItems(List<Product> updatedProducts);
+	public void calculatePrice(List<Product> updatedProducts);
 
 }
